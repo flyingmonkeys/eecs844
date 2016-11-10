f8c49a3d82556417b2530849e715f496e7f961d8
Rich Simeon <rich.simeon@garmin.com>
2016-11-09 19:56:23 -0600

    Final answer prob 1
    Change-Id: I9795f2ad0cdc5ea9e9f83240bf02dac486d688d5

--- exam3/prob1_3.m
+++ exam3/prob1_3.m
@@ -10,23 +10,8 @@ K = length(x);
 M_lp = 29; % Forward linear prediction filter order
 M_w  = 30; % Wiener filter order
 
-%rc = xcorr(d,M_lp);
-%rc1 = xcorr(d,M_lp,'coeff');
-%r = flipud(rc1(1:M_lp));
-%r1 = flipud(rc1(2:M_lp+1));
-%Rxx = toeplitz(r1);
-
-% Create D matrix
-D = zeros(M_w,K-M_w+1);
-for k=1:K-M_w+1
-    D(:,k) = flipud(d(k:k+M_w-1));
-end
-
-% Calculate Rdd
-Rdd = (1/(K-M_w+1))*D*ctranspose(D);
-
-%r(1) = var(d);
-%r = zeros(M_lp,1);
+rc = xcorr(d,M_w-1,'unbiased');
+Rdd = toeplitz(conj(flipud(rc(1:M_w))));
 r = Rdd(2:M_lp+1,1);
 
 % Calculate LP filter (w_lp)
@@ -45,19 +30,15 @@ P = P / (length(x)-M_w+1);
 % Calculate Wiener filter (ww)
 ww = inv(Rdd)*P;
 
-%a = abs(conv(w_lp,d));
-
 % Run d through LP filter
 y_lp = zeros(10000,1);
 for n=M_lp+1+1:10000
     sum = 0;
     for k=1:M_lp+1
-%        sum = sum + conj(w_lp(k))*d(n-k);
         sum = sum + conj(a(k))*d(n-k+1);
     end
     y_lp(n) = sum;
 end
-%y_lp = conv(conj(w_lp),d);
 
 % Run d through Wiener filter
 y = zeros(10000,1);
@@ -69,28 +50,31 @@ for n=M_w+1:10000
     y(n) = sum;
 end
 
-
+%% Plots--------------
 figure(1);
-plot(abs(y));
-title('Wiener filter output, M=30');
-
-figure(2);
-plot(abs(y_lp));
-title('Forward linear predictor output, M=29');
-
-figure(3);
 freqz(ww);
 title('Frequency/Phase response, Wiener filter');
 
-figure(4);
+figure(2);
 freqz(a);
 title('Frequency/Phase response, forward linear prediction filter');
 
-%for n=1:30
-%    figure(n+10);
-%    e = y(n:10000) - x(1:10000-n+1);
-%    plot(abs(e));
-%end
+figure(3);
+plot(abs(ww),'color','blue');
+grid on;
+hold on;
+plot(abs(a),'color','red');
+title('Filter Magnitude');
+legend({'Wiener filter','Forward LP'});
+
+figure(4);
+plot(angle(ww),'color','blue');
+grid on;
+hold on;
+plot(angle(a),'color','red');
+title('Filter Phase');
+legend({'Wiener filter','Forward LP'});
+
 figure(5);
 e = y - x;
 plot(abs(e));
@@ -101,4 +85,9 @@ e = y_lp - x;
 plot(abs(e));
 title('LP filter error');
 
-figure(2);
+figure(7);
+plot(abs(y));
+title('Filter output');
+hold on;
+plot(abs(y_lp),'color','red');
+legend({'Wiener','LP'});
